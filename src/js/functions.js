e1a3f628485055e15cab2abf5a85b53000ed4e46
Darth-Ness <75047141+Darth-Ness@users.noreply.github.com>
2021-12-18 14:50:47 -0500

    ReAdd Changes.

--- src/js/functions.js
+++ src/js/functions.js
@@ -3,7 +3,7 @@
  * @param {HTMLElement} tab
  * @param {string} title
  */
-function setTitle(tab, title) {
+ function setTitle(tab, title) {
   tab.children[1].innerText = title;
 }
 
@@ -203,3 +203,4 @@ function grayOut() {
     forward.classList.remove('hoverable');
   }
 }
+function searchEngine(engine) {document.getElementById("settings-searchurl").value = engine;}
