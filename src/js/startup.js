3be1fc30652524c6f0bd8a3c7f05c602e3d1584b
MystPi <blaunoah@gmail.com>
2022-02-04 14:32:12 -0500

    feat: loading screen

--- src/js/startup.js
+++ src/js/startup.js
@@ -14,6 +14,14 @@ fetch('../package.json')
   });
 
 
+setTimeout(() => {
+  byId('loading').classList.add('opacity-0');
+  setTimeout(() => {
+    byId('loading').style.display = 'none';
+  }, 500);
+}, 1000);
+
+
 if (!window.navigator.onLine) {
   byId('offline').style.display = 'block';
 }
