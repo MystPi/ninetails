a19c4af1ff55b8eaa0be13027435202713bad5d2
zani <61130784+Zxnii@users.noreply.github.com>
2022-05-05 21:26:31 -0600

    comment code

--- src/js/startup.js
+++ src/js/startup.js
@@ -17,14 +17,18 @@ window.ipc.send('toMain', 'platform');
 window.ipc.on('platform', (data) => {
   const platform = data.platform;
 
+  // ignore if not on a supported platform
   if (platform === 'win32' || platform === 'darwin') {
+    // get prefix for controls
     const prefix = platform === 'darwin' ? 'mac' : 'win';
     const titlebar = byId('titlebar');
 
+    // add padding if on windows
     if (platform === 'win32') {
       titlebar.classList.add('pl-4');
     }
 
+    // get control containers so they can be hidden or shown based on platform
     const winControls = byId('window-controls-win');
     const macControls = byId('window-controls-mac');
 
@@ -36,10 +40,12 @@ window.ipc.on('platform', (data) => {
       macControls.style.display = 'none';
     }
 
+    // get control buttons
     const close = byId(`${prefix}-close-button`);
     const minimize = byId(`${prefix}-minimize-button`);
     const maximize = byId(`${prefix}-maximize-button`);
 
+    // svg paths for controls on windows
     const winRestoreSvg = '<path d="M2.1,0v2H0v8.1h8.2v-2h2V0H2.1z M7.2,9.2H1.1V3h6.1V9.2z M9.2,7.1h-1V2H3.1V1h6.1V7.1z" style="stroke-opacity: 0;" />';
     const winMaximizeSvg = '<path d="M0,0v10.1h10.2V0H0z M9.2,9.2H1.1V1h8.1V9.2z" style="stroke-opacity: 0;" />';
 
@@ -56,6 +62,7 @@ window.ipc.on('platform', (data) => {
     });
 
     window.ipc.on('maximized', (maximized) => {
+      // if we're on windows and change the svg
       if (platform === 'win32') {
         const svg = maximize.querySelector('svg');
 
